58a0f88f4a79db0f211361092a04188a4a03aa38
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-08-08 17:01:50 +0700

    Set up app/page.tsx (home page) || Dan membuat component CompanionCard, CompanionList dan Cta || Serta Langsung Menyelesaikan CompanionCard.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,44 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import Cta from "@/components/Cta";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome To My Saas App</h1>
-      <Button>Let&apos;s Get Started</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of The Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Counsty the Number Wizard"
+          topic="Deruvatuves & Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionList />
+        <Cta />
+      </section>
+    </main>
   );
 };
 
