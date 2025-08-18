a116c9ed0ec0e1a3edeac0976b3db6d2b60025c0
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-08-18 10:08:20 +0700

    Menambahkan SENTRY yaitu software error tracking yang berfungsi untuk membantu kita dalam mengetahui melacak dan informasi lainnya jika terjadi sebuah error di program kesayangan KITA

---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://225dc203ec8f055f4e0eac2931767c19@o4509862801047552.ingest.us.sentry.io/4509862801833984",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
