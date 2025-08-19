427f56e22a40875c40c72280739c62ec6801c407
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-08-19 18:57:10 +0700

    Menyempurnakan fitur Billings pada clerk yang dimana kita akan menentukan kriteria pembuatan user berdasarkan membersif dan jika limit maka akan kita rekomendasikan untuk premium

--- lib/actions/companion.action.ts
+++ lib/actions/companion.action.ts
@@ -88,7 +88,7 @@ export const getRecentSessions = async(limit = 10) => {
         .from('session_history')
         .select(`companions:companion_id (*)`)
         .order('created_at', { ascending: false})
-        .limit(limit) //34:35
+        .limit(limit)
 
     if(error) throw new Error(error.message);
 
@@ -105,4 +105,38 @@ export const getUserCompanions = async(userId: string) => {
     if(error) throw new Error(error.message);
 
     return data;
+}
+
+
+// function untuk sinkronasi pembayaran sesuai dengan database
+export const newCompanionPermissions = async () => {
+    const {userId, has} = await auth();
+    const supabase = createSupabaseClient();
+
+    let limit = 0;
+
+        // Contoh implementasi membership dengan clerk
+    if(has({plan: 'pro'})) {
+        return true;
+    } else if (has({feature: "3_companion_limit"})) {
+        limit = 3;
+    } 
+    else if (has({feature: "10_companion_limit"})) {
+        limit = 10;
+    } 
+
+    const {data, error} = await supabase
+        .from('companions')
+        .select('id', {count: 'exact'})
+        .eq('author', userId)
+
+    if(error) throw new Error(error.message);
+
+    const companionCount = data?.length;
+
+    if(companionCount >= limit) {
+        return false
+    } else {
+        return true
+    }
 }
